7141865e9fc712a0fcb811d835c25d40cd74d441
Merak <a564178239@gmail.com>
2022-07-14 22:15:37 +0800

    Visualized quiz table

---
+++ docs/js/annotation_replacers.js
@@ -0,0 +1,78 @@
+function generate_attribute_info(arg) {
+    let tokens = arg.split(' ');
+    let texts = ['變更', [], []];
+    let modTarget = 1;
+    for(let token of tokens) {
+        switch(token.toLowerCase()) {
+            case 'loop':
+                texts[0] = '循環';
+                texts[1] = [];
+                modTarget = 1;
+                break;
+            case 'increase':
+            case 'inc':
+                texts[0] = '遞增';
+                texts[1] = [];
+                modTarget = 1;
+                break;
+            case 'decrease':
+            case 'dec':
+                texts[0] = '遞減';
+                texts[1] = [];
+                modTarget = 1;
+                break;
+            case 'init':
+                modTarget = 2;
+                texts[2] = [];
+                break;
+            default:
+                texts[modTarget].push(token);
+        }
+    }
+    return `<details><summary>屬性${texts[0]}</summary>${texts[1].join(' → ')}${texts[2].length==0 ? '' : ('<br />進場設定為'+texts[2][0])}</details>`;
+}
+
+function generate_quiz_table(arg) {
+    let table_rows_html = '';
+    let tokens = arg.split(' ');
+    if(tokens.length < 2) {
+        return `<details><summary>@quiz</summary>無資料</details>`
+    }
+    tokens.forEach((index, element) => {
+        tokens[index] = element.split(',');
+    });
+    tokens[1].forEach(element => {
+        let qa = quiz_data.quiz[element];
+        let question = qa.question;
+        let answer = `<code>#${tokens[0][qa.answer]}</code>`;
+        if(tokens[0][qa.answer] < 50000) answer = `<img src="https://tinghan33704.github.io/tos_tool_data/img/monster/${tokens[0][qa.answer]}.png" />`;
+        //let answer = `<img src="/tool_data/image/monsters/${tokens[0][qa.answer]}.png" />`;
+        table_rows_html += `<tr><td>${question}</td><td>${answer}</td></tr>`;
+    })
+    return `<details><summary>@quiz</summary><table class="quiz"><thead><tr><th>問題</th><th>答案</th></tr></thead><tbody>${table_rows_html}</tbody></table></details>`;
+}
+
+const annotation_replacers = {
+    'crescendo': arg => `<details><summary>越戰越強</summary><table><tr><td>剩餘血量</td><td>100%</td><td>20%</td><td>0%</td></tr><tr><td>倍率</td><td>1</td><td>2</td><td>2</td></tr></table><ul><li>各點之間為線性成長</li></ul></details>`,
+    'crescendo_ex': arg => `<details><summary>越戰越強</summary><table><tr><td>剩餘血量</td><td>100%</td><td>20%</td><td>0%</td></tr><tr><td>倍率</td><td>1</td><td>3</td><td>3</td></tr></table><ul><li>各點之間為線性成長</li></ul></details>`,
+    'trojan': arg => `<details><summary>越戰越強</summary><table><tr><td>剩餘血量</td><td>100%</td><td>50%</td><td>35%</td><td>20%</td><td>0%</td></tr><tr><td>倍率</td><td>1</td><td>2</td><td>5</td><td>10</td><td>10</td></tr></table><ul><li>各點之間為線性成長</li><li>血量30%以下時發動連擊</li></ul></details>`,
+    'trojan_ex': arg => `<details><summary>越戰越強</summary><table><tr><td>剩餘血量</td><td>100%</td><td>50%</td><td>35%</td><td>20%</td><td>0%</td></tr><tr><td>倍率</td><td>1</td><td>3</td><td>10</td><td>20</td><td>20</td></tr></table><ul><li>各點之間為線性成長</li><li>血量30%以下時發動連擊</li></ul></details>`,
+    'trojan_fort_ex': arg => `<details><summary>越戰越強</summary><table><tr><td>剩餘血量</td><td>100%</td><td>50%</td><td>35%</td><td>20%</td><td>0%</td></tr><tr><td>倍率</td><td>1</td><td>3</td><td>10</td><td>20</td><td>20</td></tr></table><ul><li>各點之間為線性成長</li><li>攻擊、防禦倍率相同</li><li>血量30%以下時發動連擊</li></ul></details>`,
+    'trojan_multi_attack': arg => `<details><summary>血低追擊</summary><table><tr><td>剩餘血量</td><td>100%</td><td>80%</td><td>60%</td><td>40%</td><td>20%</td></tr><tr><td>攻擊次數</td><td>+0</td><td>+1</td><td>+2</td><td>+3</td><td>+4</td></tr></table></details>`,
+    'trojan_attack': arg => `<details><summary>越攻越強</summary><table><tr><td>攻擊次數</td><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td></tr><tr><td>倍率</td><td>1</td><td>2</td><td>4</td><td>8</td><td>16</td><td>32</td></tr></table><ul><li>第6次攻擊起會連擊，並停止倍率成長</li></ul></details>`,
+    'trojan_hp_attack': arg => `<details><summary>越扣越強</summary>${arg=='' ? '無資料' : (arg.replaceAll(' ', '% → ')+'%')}</details>`,
+    'explode_increase_attack': arg => `<details><summary>引爆增攻</summary>每1顆增加0.5倍，最高20顆增加10倍</details>`,
+    'explode_reduce_damage': arg => `<details><summary>引爆減傷</summary>每1顆減傷${arg || '5'}%，最高${Math.ceil(100/parseInt(arg || '5'))}顆減傷100%</details>`,
+    'mild_attack': arg => `<details><summary>輕量攻擊</summary>敵人攻擊力的65%</details>`,
+    'ambush': arg => `<details><summary>突擊</summary>${arg=='' ? '無資料' : '敵人攻擊力的'+arg+'%'}</details>`,
+    'fixed_board': arg => `<details><summary>固定版面</summary>${get_fixed_board(arg)}</details>`,
+    'fixed_shape': arg => `<details><summary>固定版面</summary>${get_fixed_shape(arg)}</details>`,
+    'fixed_position': arg => `<details><summary>固定位置</summary>${get_fixed_position(arg)}</details>`,
+    'crumbling_walls': arg => `<details><summary>減傷抗性</summary>${arg=='' ? '無資料' : (arg.replaceAll(' ', '% → ')+'%')}</details>`,
+    'assigned_combo': arg => `<details><summary>指定連擊</summary>${arg=='' ? '無資料' : (arg.replaceAll(' ', ' Combo → ')+' Combo')}</details>`,
+    'random_combo': arg => `<details><summary>指定連擊</summary>${arg=='' ? '無資料' : ('隨機範圍 '+arg.replace(' ', ' Combo ~ ')+' Combo')}</details>`,
+    'assigned_number': arg => `<details><summary>指定數量</summary>${arg=='' ? '無資料' : (arg.replaceAll(' ', ' → '))}</details>`,
+    'random_number': arg => `<details><summary>指定數量</summary>${arg=='' ? '無資料' : ('隨機範圍 '+arg.replace(' ', ' ~ '))}</details>`,
+    'attribute_change': generate_attribute_info,
+    'quiz': generate_quiz_table
+}
--- docs/js/filter.js
+++ docs/js/filter.js
@@ -63,63 +63,6 @@ function clear_table() {
     }
 }
 
-const annotation_replacers = {
-    'crescendo': arg => `<details><summary>越戰越強</summary><table><tr><td>剩餘血量</td><td>100%</td><td>20%</td><td>0%</td></tr><tr><td>倍率</td><td>1</td><td>2</td><td>2</td></tr></table><ul><li>各點之間為線性成長</li></ul></details>`,
-    'crescendo_ex': arg => `<details><summary>越戰越強</summary><table><tr><td>剩餘血量</td><td>100%</td><td>20%</td><td>0%</td></tr><tr><td>倍率</td><td>1</td><td>3</td><td>3</td></tr></table><ul><li>各點之間為線性成長</li></ul></details>`,
-    'trojan': arg => `<details><summary>越戰越強</summary><table><tr><td>剩餘血量</td><td>100%</td><td>50%</td><td>35%</td><td>20%</td><td>0%</td></tr><tr><td>倍率</td><td>1</td><td>2</td><td>5</td><td>10</td><td>10</td></tr></table><ul><li>各點之間為線性成長</li><li>血量30%以下時發動連擊</li></ul></details>`,
-    'trojan_ex': arg => `<details><summary>越戰越強</summary><table><tr><td>剩餘血量</td><td>100%</td><td>50%</td><td>35%</td><td>20%</td><td>0%</td></tr><tr><td>倍率</td><td>1</td><td>3</td><td>10</td><td>20</td><td>20</td></tr></table><ul><li>各點之間為線性成長</li><li>血量30%以下時發動連擊</li></ul></details>`,
-    'trojan_fort_ex': arg => `<details><summary>越戰越強</summary><table><tr><td>剩餘血量</td><td>100%</td><td>50%</td><td>35%</td><td>20%</td><td>0%</td></tr><tr><td>倍率</td><td>1</td><td>3</td><td>10</td><td>20</td><td>20</td></tr></table><ul><li>各點之間為線性成長</li><li>攻擊、防禦倍率相同</li><li>血量30%以下時發動連擊</li></ul></details>`,
-    'trojan_multi_attack': arg => `<details><summary>血低追擊</summary><table><tr><td>剩餘血量</td><td>100%</td><td>80%</td><td>60%</td><td>40%</td><td>20%</td></tr><tr><td>攻擊次數</td><td>+0</td><td>+1</td><td>+2</td><td>+3</td><td>+4</td></tr></table></details>`,
-    'trojan_attack': arg => `<details><summary>越攻越強</summary><table><tr><td>攻擊次數</td><td>1</td><td>2</td><td>3</td><td>4</td><td>5</td><td>6</td></tr><tr><td>倍率</td><td>1</td><td>2</td><td>4</td><td>8</td><td>16</td><td>32</td></tr></table><ul><li>第6次攻擊起會連擊，並停止倍率成長</li></ul></details>`,
-    'trojan_hp_attack': arg => `<details><summary>越扣越強</summary>${arg=='' ? '無資料' : (arg.replaceAll(' ', '% → ')+'%')}</details>`,
-    'explode_increase_attack': arg => `<details><summary>引爆增攻</summary>每1顆增加0.5倍，最高20顆增加10倍</details>`,
-    'explode_reduce_damage': arg => `<details><summary>引爆減傷</summary>每1顆減傷${arg || '5'}%，最高${Math.ceil(100/parseInt(arg || '5'))}顆減傷100%</details>`,
-    'mild_attack': arg => `<details><summary>輕量攻擊</summary>敵人攻擊力的65%</details>`,
-    'ambush': arg => `<details><summary>突擊</summary>${arg=='' ? '無資料' : '敵人攻擊力的'+arg+'%'}</details>`,
-    'fixed_board': arg => `<details><summary>固定版面</summary>${get_fixed_board(arg)}</details>`,
-    'fixed_shape': arg => `<details><summary>固定版面</summary>${get_fixed_shape(arg)}</details>`,
-    'fixed_position': arg => `<details><summary>固定位置</summary>${get_fixed_position(arg)}</details>`,
-    'crumbling_walls': arg => `<details><summary>減傷抗性</summary>${arg=='' ? '無資料' : (arg.replaceAll(' ', '% → ')+'%')}</details>`,
-    'assigned_combo': arg => `<details><summary>指定連擊</summary>${arg=='' ? '無資料' : (arg.replaceAll(' ', ' Combo → ')+' Combo')}</details>`,
-    'random_combo': arg => `<details><summary>指定連擊</summary>${arg=='' ? '無資料' : ('隨機範圍 '+arg.replace(' ', ' Combo ~ ')+' Combo')}</details>`,
-    'assigned_number': arg => `<details><summary>指定數量</summary>${arg=='' ? '無資料' : (arg.replaceAll(' ', ' → '))}</details>`,
-    'random_number': arg => `<details><summary>指定數量</summary>${arg=='' ? '無資料' : ('隨機範圍 '+arg.replace(' ', ' ~ '))}</details>`,
-    'attribute_change': arg => {
-        let tokens = arg.split(' ');
-        let texts = ['變更', [], []];
-        let modTarget = 1;
-        for(let token of tokens) {
-            switch(token.toLowerCase()) {
-                case 'loop':
-                    texts[0] = '循環';
-                    texts[1] = [];
-                    modTarget = 1;
-                    break;
-                case 'increase':
-                case 'inc':
-                    texts[0] = '遞增';
-                    texts[1] = [];
-                    modTarget = 1;
-                    break;
-                case 'decrease':
-                case 'dec':
-                    texts[0] = '遞減';
-                    texts[1] = [];
-                    modTarget = 1;
-                    break;
-                case 'init':
-                    modTarget = 2;
-                    texts[2] = [];
-                    break;
-                default:
-                    texts[modTarget].push(token);
-            }
-        }
-        return `<details><summary>屬性${texts[0]}</summary>${texts[1].join(' → ')}${texts[2].length==0 ? '' : ('<br />進場設定為'+texts[2][0])}</details>`;
-    },
-    'quiz': arg => `<details><summary>@quiz</summary>${arg}</details>`
-}
-
 function create_row(id) {
     if(id in es_data.es) {
         let new_row = es_table.insertRow(-1);
@@ -170,8 +113,8 @@ function searchES() {
 let es_data = {
     'last_update': {
         'version': '-',
-        'date': '2022-11-06T09:30:00.000Z',
-        'dv': '-'
+        'dv': '-',
+        'time': 0
     },
     'es': {
         '0': {
@@ -181,8 +124,17 @@ let es_data = {
         }
     }
 };
+let quiz_data = {
+    'last_update': {
+        'version': '-',
+        'dv': '-',
+        'time': 0
+    },
+    'quiz': {}
+};
 
 $(async function() {
+    /*
     await new Promise((resolve, reject) => {
         $.ajax({
             'type': 'GET',
@@ -196,7 +148,10 @@ $(async function() {
                 reject();
             }
         });
-    });
+    }); */
+
+    es_data = await fetch('/tool_data/data/es.json').then(res => res.json());
+    quiz_data = await fetch('/tool_data/data/quiz.json').then(res => res.json());
 
     const update_date = new Date(es_data.last_update.time * 1000);
     document.getElementById('last_update_version').innerText = `${es_data.last_update.version}`;
